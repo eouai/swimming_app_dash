0b2b2809c4890315a574d97b0150f9fb97f9ff49
BurtG-rem <burtg@opensource.gov>
2018-04-26 20:39:50 +0000

    add overall class selection to graph filters

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ Created on Tue Apr 10 17:15:48 2018
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table_experiments as dte
+#import dash_table_experiments as dte
 #from dash.dependencies import Input, Output
 import pandas as pd
 import re
 import numpy as np
-from datetime import time
 import datetime
 import plotly.graph_objs as go
 
@@ -86,19 +85,28 @@ def to_unix_time(dt):
     epoch = datetime.datetime.utcfromtimestamp(0)
     return (dt - epoch).total_seconds() * 1000 
 
-def get_date_vars(df):
+def get_date_vars(df, selected_class):
+    #year, month, day values of time objects are not needed
+    #swimming results only require min:sec.sec data
+    #defaulting all time objects to 2000-01-01
     #timestamp = datetime.date.today()
     year_val = 2000 #timestamp.year
     mon_val = 1 #timestamp.month
     day_val = 1 #timestamp.day
-    hour_val = 5
+    hour_val = 0
     min_val_min = int(df.sort_values('time_seconds')['time_seconds'].head(1))//60
     sec_val_min = int(df.sort_values('time_seconds')
         ['time_seconds'].head(1)%60)-1
-    min_val_max = int(df.sort_values('seed_time_seconds',ascending=False)
-        ['seed_time_seconds'].head(1))//60
-    sec_val_max = int(df.sort_values('seed_time_seconds',ascending=False)
-        ['seed_time_seconds'].head(1)%60)+2
+    if len(selected_class) == 1:
+        min_val_max = int(df.sort_values('seed_time_seconds',ascending=False)
+            ['seed_time_seconds'].head(1))//60
+        sec_val_max = int(df.sort_values('seed_time_seconds',ascending=False)
+            ['seed_time_seconds'].head(1)%60)+2
+    else:
+        min_val_max = int(df.sort_values('time_seconds').groupby('date_year') \
+            .time_seconds.nth(15).max()//60)
+        sec_val_max = int(df.sort_values('time_seconds').groupby('date_year') \
+            .time_seconds.nth(15).max()%60+4)      
     if sec_val_min < 0:
         min_val_min = min_val_min - 1
         sec_val_min = sec_val_min + 60
@@ -125,11 +133,12 @@ gender = [{'label': 'M', 'value': 'M'},
 performer = [{'label': 'Top Performances', 'value': 'Performances'},
              {'label': 'Top Performers', 'value': 'Performers'}]
 
-classes = [{'label': '6A', 'value': '6A'},
-           {'label': '5A', 'value': '5A'},
-           {'label': '4A', 'value': '4A'},
-           {'label': '3A', 'value': '3A'},
-           {'label': '2A', 'value': '2A'},]
+classes = [{'label': '6A', 'value': ['6A']},
+           {'label': '5A', 'value': ['5A']},
+           {'label': '4A', 'value': ['4A']},
+           {'label': '3A', 'value': ['3A']},
+           {'label': '2A', 'value': ['2A']},
+           {'label': 'Overall', 'value': ['6A','5A','4A','3A','2A']}]
 
 
 df_SV = cleanup_SV(df_SV_raw)
@@ -211,7 +220,7 @@ page_2_layout = html.Div([
             options = classes,
             value='' #gender[0]
             )],
-        style = dict(width = '35%',
+        style = dict(width = '45%',
                 display = 'table-cell',
                      ),
         ),
@@ -227,25 +236,29 @@ page_2_layout = html.Div([
          dash.dependencies.Input('gender-selection', 'value'),
          dash.dependencies.Input('class-selection', 'value')])
 def update_figure(selected_event, selected_gender, selected_class):
+    
     filtered_df_State = pd.DataFrame(df_State[df_State['Event'] == selected_event])
-    filtered_df_State = pd.DataFrame(filtered_df_State[filtered_df_State['Gender'] 
-        == selected_gender])
-    filtered_df_State = pd.DataFrame(filtered_df_State[filtered_df_State['Class'] 
-        == selected_class])
-    year_val, mon_val, day_val, hour_val, min_val_min, sec_val_min, min_val_max, sec_val_max = get_date_vars(filtered_df_State)
+    filtered_df_State = filtered_df_State[filtered_df_State['Gender'] == selected_gender]
+    filtered_df_State = filtered_df_State[filtered_df_State['Class'].isin(selected_class)]
+    print("test")
+    year_val, mon_val, day_val, hour_val, min_val_min, sec_val_min \
+        ,min_val_max, sec_val_max = get_date_vars(filtered_df_State, selected_class)
     y_axis_min = to_unix_time(datetime.datetime(year_val,mon_val,day_val,hour_val,
                                                 min_val_min,sec_val_min))
     y_axis_max = to_unix_time(datetime.datetime(year_val,mon_val,day_val,hour_val,
                                                 min_val_max,sec_val_max))
-    return generate_figure(filtered_df_State, y_axis_min, y_axis_max)
+    print(y_axis_min, y_axis_max)
+    print(filtered_df_State.sort_values('time_seconds').groupby('date_year').time_obj.nth(15))
+    return generate_figure(filtered_df_State, y_axis_min, y_axis_max, selected_event, selected_gender, selected_class)
 
-def generate_figure(filtered_df_State, y_axis_min, y_axis_max):
+def generate_figure(filtered_df_State, y_axis_min, y_axis_max, selected_event, selected_gender, selected_class):
     return {
       'data': [
           go.Scatter(
-              x=np.sort(filtered_df_State['date_year'].unique()),
-              y=filtered_df_State.groupby('date_year').seed_time_obj.agg('max'),
-              text=((filtered_df_State.sort_values(
+              x=(np.sort(filtered_df_State['date_year'].unique()) if len(selected_class) == 1 else []),
+              y=(filtered_df_State.groupby('date_year').seed_time_obj.agg('max') if 
+                 len(selected_class) == 1 else []),
+              text=(((filtered_df_State.sort_values(
                   ['date_year', 'seed_time_seconds'], ascending=[False, False])
                      .groupby('date_year')[['Swimmer', 'seed_time_obj', 'seed_min_obj']]
                      .head(1)['seed_min_obj'].apply(lambda x: x[:5]).sort_index(ascending=False)).str.cat(
@@ -254,15 +267,15 @@ def generate_figure(filtered_df_State, y_axis_min, y_axis_max):
                    .groupby('date_year')[['Swimmer', 'seed_time_obj', 'seed_min_obj']]
                    .head(1)['seed_min_obj'].apply(lambda x: x[-2:])).sort_index(ascending=False),
                   sep='.'
-              )),
+              )) if len(selected_class) == 1 else ""),
               mode='lines+markers',
               name='State Qualifying Time',
               hoverinfo='text'
           ),
           go.Scatter(
               x=np.sort(filtered_df_State['date_year'].unique()),
-              y=filtered_df_State.groupby('date_year').time_obj.nth(15),
-              text=(filtered_df_State.sort_values(
+              y=filtered_df_State.sort_values('time_seconds').groupby('date_year').time_obj.nth(15),
+              text=((filtered_df_State.sort_values(
                   ['date_year', 'time_seconds'], ascending=[False, True])
                   .groupby('date_year')[['Swimmer', 'time_obj', 'min_obj']]
                   .nth(15)['Swimmer']).sort_index(ascending=True).str.cat(
@@ -277,15 +290,15 @@ def generate_figure(filtered_df_State, y_axis_min, y_axis_max):
                    .groupby('date_year')[['Swimmer', 'time_obj', 'min_obj']]
                    .nth(15)['min_obj'].apply(lambda x: x[-2:])).sort_index(ascending=True),
                   sep='.'
-              ),
+              )),
               mode='lines+markers',
               name='16th Place',
               hoverinfo='text'
           ),
           go.Scatter(
               x=np.sort(filtered_df_State['date_year'].unique()),
-              y=filtered_df_State.groupby('date_year').time_obj.nth(7),
-              text=(filtered_df_State.sort_values(
+              y=filtered_df_State.sort_values('time_seconds').groupby('date_year').time_obj.nth(7),
+              text=((filtered_df_State.sort_values(
                   ['date_year', 'time_seconds'], ascending=[False, True])
                   .groupby('date_year')[['Swimmer', 'time_obj', 'min_obj']]
                   .nth(7)['Swimmer']).sort_index(ascending=True).str.cat(
@@ -300,15 +313,15 @@ def generate_figure(filtered_df_State, y_axis_min, y_axis_max):
                    .groupby('date_year')[['Swimmer', 'time_obj', 'min_obj']]
                    .nth(7)['min_obj'].apply(lambda x: x[-2:])).sort_index(ascending=True),
                   sep='.'
-              ),
+              )),
               mode='lines+markers',
               name='8th Place',
               hoverinfo='text'
           ),
           go.Scatter(
               x=np.sort(filtered_df_State['date_year'].unique()),
-              y=filtered_df_State.groupby('date_year').time_obj.agg('min'),
-              text=(filtered_df_State[filtered_df_State['Place'] == 1].sort_values(
+              y=filtered_df_State.sort_values('time_seconds').groupby('date_year').time_obj.nth(0),
+              text=((filtered_df_State[filtered_df_State['Place'] == 1].sort_values(
                     ['date_year','time_seconds'],ascending=[False,True])
                   .groupby('date_year')[['Swimmer','time_obj','min_obj']]
                   .head(1)['Swimmer']).sort_index(ascending=False).str.cat(
@@ -323,7 +336,7 @@ def generate_figure(filtered_df_State, y_axis_min, y_axis_max):
                   .groupby('date_year')[['Swimmer','time_obj','min_obj']]
                   .head(1)['min_obj'].apply(lambda x: x[-2:])).sort_index(ascending=False),
                   sep='.'
-          ),
+          )),
           mode='lines+markers',
           name='State Champion',
           hoverinfo='text'
@@ -335,7 +348,8 @@ def generate_figure(filtered_df_State, y_axis_min, y_axis_max):
                  'tickformat': '%M:%S.%2f'},
           xaxis={'title': 'Year',
                  'dtick': 1},
-          hovermode='closest')
+          hovermode='closest'
+          )
       }
     
 def generate_table(df, max_rows=10):
